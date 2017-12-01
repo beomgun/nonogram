7e0ea71abeb823ab6acdabba2aaa8607da15264b
beomgun <qjarjs729@naver.com>
2017-12-01 17:52:47 +0900

    초기 Rank

---
+++ Rank_Button.java
@@ -0,0 +1,93 @@
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+//class rank_table{
+//	JFrame Rank_Frame = new JFrame();
+//	String columnNames[] = {"순위","이름","시간","난이도"};
+//	String rowData[][] = {
+//			{"1", "A","15:00","Hard"},
+//			{"2", "B","14:00","Normal"},
+//			{"3", "C","13:00","Easy"}
+//	};
+//	JTable Rank_table = new JTable(rowData,columnNames);
+//	JScrollPane jscrollpane = new JScrollPane(Rank_table);
+//	
+//	public rank_table() {
+//		Rank_Frame.add(jscrollpane);
+//		
+//		Rank_Frame.setSize(600,300);
+//		Rank_Frame.setVisible(true);
+//		Rank_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+//	}
+//	
+//}
+public class Rank_Button extends JFrame{
+	private static final long serialVersionUID = 1L;
+	private JButton rToMain;
+	
+	public Rank_Button(String title) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700,700);
+		setTitle(title);
+		
+		rToMain = new JButton("메인으로");
+		
+		Dimension d = rToMain.getPreferredSize();
+		rToMain.setSize(700/5,700/10 );
+		rToMain.setLocation(700/17,(int)(700/8));
+		
+		JFrame Rank_Frame = new JFrame();
+		String columnNames[] = {"순위","이름","시간","난이도"};
+		String rowData[][] = {
+				{"1", "A","15:00","Hard"},
+				{"2", "B","14:00","Normal"},
+				{"3", "C","13:00","Easy"}
+		};
+		JTable Rank_table = new JTable(rowData,columnNames);
+		JScrollPane jscrollpane = new JScrollPane(Rank_table);
+		Rank_Frame.add(jscrollpane);
+		
+		Rank_Frame.setSize(600,300);
+		Rank_Frame.setVisible(true);
+		Rank_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLayout(null);
+		add(rToMain);
+		add(Rank_table);
+
+
+		setVisible(true);
+		
+	}
+	
+
+//	public void addButton() {
+//		JButton btn = new JButton("메인으로");
+//		setLayout(null);
+//		add(btn);
+//		setVisible(true);
+//		
+//	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rank_Button rB = new Rank_Button("Ranking");
+		JFrame Rank_Frame = new JFrame();
+		String columnNames[] = {"순위","이름","시간","난이도"};
+		String rowData[][] = {
+				{"1", "A","15:00","Hard"},
+				{"2", "B","14:00","Normal"},
+				{"3", "C","13:00","Easy"}
+		};
+		JTable Rank_table = new JTable(rowData,columnNames);
+		JScrollPane jscrollpane = new JScrollPane(Rank_table);
+		Rank_Frame.add(jscrollpane);
+		
+		Rank_Frame.setSize(600,300);
+		Rank_Frame.setVisible(true);
+		Rank_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
